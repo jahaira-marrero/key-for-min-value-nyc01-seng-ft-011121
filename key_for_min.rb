28bbcc7630990140b9590ccfe821a1c49a5a8305
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-01-05 22:19:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,14 +11,8 @@ def key_for_min_value(name_hash)
       lowest_value = value
       lowest_key = key
     
-    # new_hash.each do |key, value|
-    # if lowest_value == nil || value < lowest_value
-    #   lowest_value == value
-    #   lowest_key == key
-    # end
   end
 end
   end
-  lowest_key
   lowest_key
 end
